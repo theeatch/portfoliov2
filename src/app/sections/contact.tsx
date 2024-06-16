eacd1fba4368a6951ffae0d34089b6e537697877
theeatch <dhruvnegiclub03@gmail.com>
2024-06-16 09:47:13 +0530

    fix email message in contact

--- src/app/sections/contact.tsx
+++ src/app/sections/contact.tsx
@@ -1,9 +1,8 @@
 "use client";
 import Footer from "@/components/footer";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
-import { useRef, useState } from "react";
 
 type Props = {};
 
@@ -38,64 +37,77 @@ const Contact = (props: Props) => {
         );
     }
   };
+
   return (
     <div className="w-full h-screen space-y-10">
-        <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 pb-40 font-semibold">
-          {/* TEXT CONTAINER */}
-          <div className="hidden h-1/2 lg:h-full lg:w-1/2 md:flex items-center justify-center text-4xl lg:text-7xl underline">
-            <div>
-              {text.split("").map((letter, index) => (
-                <motion.span
-                  key={index}
-                  initial={{ opacity: 1 }}
-                  animate={{ opacity: 0 }}
-                  transition={{
-                    duration: 8,
-                    repeat: Infinity,
-                    delay: index * 0.4,
-                  }}
-                >
-                  {letter}
-                </motion.span>
-              ))}
-            </div>
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 pb-40 font-semibold">
+        {/* TEXT CONTAINER */}
+        <div className="hidden h-1/2 lg:h-full lg:w-1/2 md:flex items-center justify-center text-4xl lg:text-7xl underline">
+          <div>
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 8,
+                  repeat: Infinity,
+                  delay: index * 0.4,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
           </div>
-          {/* FORM CONTAINER */}
-          <form
-            onSubmit={sendEmail}
-            ref={form}
-            className="h-full sm:h-1/2 my-auto md:h-4/5 lg:w-1/2 bg-slate-700 text-white rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
-          >
+        </div>
+        {/* FORM CONTAINER */}
+        <form
+          onSubmit={sendEmail}
+          ref={form}
+          className="h-full sm:h-1/2 my-auto md:h-4/5 lg:w-1/2 bg-slate-700 text-white rounded-xl text-xl flex flex-col gap-8 justify-center p-20"
+        >
+          <div className="h-1/2 flex-col flex gap-8">
             <span>Dear DH,</span>
             <textarea
-              className="bg-transparent border-b-2 border-b-black outline-none resize-none"
-              name="user_message"
+              className="bg-transparent border-b-2 border-b-black outline-none h-full"
+              name="message"
             />
+          </div>
+          <div className="h-1/4 flex-col flex">
+            <span>My Name:</span>
+            <input
+              name="from_name"
+              type="text"
+              className="bg-transparent border-b-2 border-b-black outline-none"
+            />
+          </div>
+          <div className="h-1/4 flex-col flex">
             <span>My mail address is:</span>
             <input
               name="user_email"
               type="email"
               className="bg-transparent border-b-2 border-b-black outline-none"
             />
-            <span>Regards</span>
-            <button
-              type="submit"
-              className="bg-purple-200 rounded font-semibold text-gray-600 p-4"
-            >
-              Send
-            </button>
-            {success && (
-              <span className="text-green-600 font-semibold">
-                Your message has been sent successfully!
-              </span>
-            )}
-            {error && (
-              <span className="text-red-600 font-semibold">
-                Something went wrong!
-              </span>
-            )}
-          </form>
-        </div>
+          </div>
+          <span>Regards</span>
+          <button
+            type="submit"
+            className="bg-purple-200 rounded font-semibold text-gray-600 p-4"
+          >
+            Send
+          </button>
+          {success && (
+            <span className="text-green-600 font-semibold">
+              Your message has been sent successfully!
+            </span>
+          )}
+          {error && (
+            <span className="text-red-600 font-semibold">
+              Something went wrong!
+            </span>
+          )}
+        </form>
+      </div>
       <Footer />
     </div>
   );
